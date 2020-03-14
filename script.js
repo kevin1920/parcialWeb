1ebeee76082d585bcdb41badb6a34d28fb26851a
kevin1920 <56529861+kevin1920@users.noreply.github.com>
2020-03-14 15:32:47 -0500

    Avance interfaz

---
+++ script.js
@@ -0,0 +1,11 @@
+let usuarios = []
+
+function atraparDatos(){
+    let tipoDocumento = document.getElementById("cmbIdentificacion").value
+    let identificacion = document.getElementById("txtId").value
+    let nombres = document.getElementById("txtNombres").value
+    let apellidos = document.getElementById("txtApellidos").value
+    let correo = document.getElementById("txtEmail").value
+    let peso = document.getElementById("txtPeso").value
+    let estatura = document.getElementById("txtEstatura").value
+}
